f4afa2e261b131e5ebf5d62af733aff553e55747
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-29 10:59:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return window.kittens.push(name);
+}
+
+
+function destructivelyPrependKitten(name) {
+  return window.kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return window.kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return window.kittens.shift();
+}
+
+function appendKitten(name) {
+  var newarr = [...window.kittens];
+  newarr.push(name);
+  return newarr;
+}
+
+
+function prependKitten(name) {
+  var newarr = [...window.kittens];
+  newarr.unshift(name);
+  return newarr;
+}
+
+function removeLastKitten() {
+  var newarr = [...window.kittens];
+  newarr.pop();
+  return newarr;
+}
+
+function removeFirstKitten() {
+  var newarr = [...window.kittens];
+  newarr.shift();
+  return newarr;
+}
