5b66dc02fdd5c2d7f217319299db9e2d377dc477
Flatiron School <github email address>
2016-10-26 17:26:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 const app = "I don't do much."
+
+
+const kittens = ["Milo", "Otis", "Garfield"]
+
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+
+  return kittens
+}
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+
+  return kittens
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+
+function appendKitten(name) {
+
+
+ var kats = [...kittens, name]
+
+  // debugger;
+  return kats
+}
+
+
+
+function prependKitten(name) {
+  var kats = [name, ...kittens]
+
+  return kats
+}
+
+
+function removeLastKitten() {
+  var kats = kittens.slice(0, kittens.length - 1)
+
+
+  return kats
+}
+
+
+function removeFirstKitten() {
+  var kats = kittens.slice(1)
+
+  return kats
+}
